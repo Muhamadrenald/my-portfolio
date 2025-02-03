bab898b75de2ab170d6a79c41d67200f2b2cad1a
Muhamad Renald <avryso@gmail.com>
2025-02-03 09:54:02 +0700

    feat: adds some code in App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Hero from "./components/hero/Hero.jsx";
-import Portfolio from "./components/portfolio/Portfolio.jsx";
 import Services from "./components/services/Services.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
 import Contact from "./components/contact/Contact.jsx";
 
 const App = () => {
   return (
-    <div className="">
-      <Hero />
-      <Portfolio />
-      <Services />
-      <Contact />
+    <div className="container">
+      <section id="#home">
+        <Hero />
+      </section>
+      <section id="#services">
+        <Services />
+      </section>
+      <section id="#portfolio">
+        <Portfolio />
+        <section id="#contact">
+          <Contact />
+        </section>
+      </section>
     </div>
   );
 };
